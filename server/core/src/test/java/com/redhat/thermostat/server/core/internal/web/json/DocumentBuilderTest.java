25b43490598bcba42fb2f7dfc327ccd88b97a104
Jie Kang <jkang@redhat.com>
2017-02-23 15:53:51 -0500

    Add more tests for DocumentBuilder

--- server/core/src/test/java/com/redhat/thermostat/server/core/internal/web/json/DocumentBuilderTest.java
+++ server/core/src/test/java/com/redhat/thermostat/server/core/internal/web/json/DocumentBuilderTest.java
@@ -15,4 +15,25 @@ public class DocumentBuilderTest {
         String expected = "{\"a\":\"b\",\"tags\":[\"admin\",\"x\",\"y\"]}";
         assertEquals(expected, result);
     }
+
+    @Test
+    public void testAddOneTag() {
+        String content = "{\"a\":\"b\"}";
+        String[] tags = {"x"};
+
+        String result = DocumentBuilder.addTags(content, tags);
+
+        String expected = "{\"a\":\"b\",\"tags\":[\"admin\",\"x\"]}";
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testAddZeroTags() {
+        String content = "{\"a\":\"b\"}";
+
+        String result = DocumentBuilder.addTags(content);
+
+        String expected = "{\"a\":\"b\",\"tags\":[\"admin\"]}";
+        assertEquals(expected, result);
+    }
 }
