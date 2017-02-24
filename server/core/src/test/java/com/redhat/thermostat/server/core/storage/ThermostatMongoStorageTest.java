d963db75b2c54d95871d8dc12a80842e441c8667
Jie Kang <jkang@redhat.com>
2017-02-24 10:43:56 -0500

    Remove unnecessary @Ignore annotation from ThermostatMongoStorageTest

--- server/core/src/test/java/com/redhat/thermostat/server/core/storage/ThermostatMongoStorageTest.java
+++ server/core/src/test/java/com/redhat/thermostat/server/core/storage/ThermostatMongoStorageTest.java
@@ -6,7 +6,6 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import com.redhat.thermostat.server.core.internal.storage.ThermostatMongoStorage;
