748da20db5d546354ff0d7a2df129a2ff25665db
Jie Kang <jkang@redhat.com>
2017-03-02 12:08:26 -0500

    Add more tests for accessing http paths. Rename to CoreServerHttpTest

---
+++ server/core/src/test/java/com/redhat/thermostat/server/core/CoreServerHttpTest.java
@@ -0,0 +1,98 @@
+package com.redhat.thermostat.server.core;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
+import javax.ws.rs.core.Response;
+
+import org.eclipse.jetty.client.api.ContentResponse;
+import org.eclipse.jetty.http.HttpMethod;
+import org.junit.Test;
+
+public class CoreServerHttpTest extends AbstractCoreServerTest {
+    @Test
+    public void testGetSystems() throws InterruptedException, ExecutionException, TimeoutException {
+        String url = baseUrl + "/namespace/systems/systemId";
+        ContentResponse response = client.newRequest(url).method(HttpMethod.GET).send();
+        assertTrue(response.getStatus() == Response.Status.SERVICE_UNAVAILABLE.getStatusCode());
+    }
+
+    @Test
+    public void testGetAgents() throws InterruptedException, ExecutionException, TimeoutException {
+        String url = baseUrl + "/namespace/systems/systemId/agents/agentId";
+        ContentResponse response = client.newRequest(url).method(HttpMethod.GET).send();
+        assertTrue(response.getStatus() == Response.Status.SERVICE_UNAVAILABLE.getStatusCode());
+    }
+
+    @Test
+    public void testGetJvms() throws InterruptedException, ExecutionException, TimeoutException {
+        String url = baseUrl + "/namespace/systems/systemId/agents/agentId/jvms/jvmId";
+        ContentResponse response = client.newRequest(url).method(HttpMethod.GET).send();
+        assertTrue(response.getStatus() == Response.Status.SERVICE_UNAVAILABLE.getStatusCode());
+    }
+
+    @Test
+    public void testPutSystems() throws InterruptedException, ExecutionException, TimeoutException {
+        String url = baseUrl + "/namespace/systems/systemId";
+        ContentResponse response = client.newRequest(url).method(HttpMethod.PUT).send();
+        assertTrue(response.getStatus() == Response.Status.SERVICE_UNAVAILABLE.getStatusCode());
+    }
+
+    @Test
+    public void testPutAgents() throws InterruptedException, ExecutionException, TimeoutException {
+        String url = baseUrl + "/namespace/systems/systemId/agents/agentId";
+        ContentResponse response = client.newRequest(url).method(HttpMethod.PUT).send();
+        assertTrue(response.getStatus() == Response.Status.SERVICE_UNAVAILABLE.getStatusCode());
+    }
+
+    @Test
+    public void testPutJvms() throws InterruptedException, ExecutionException, TimeoutException {
+        String url = baseUrl + "/namespace/systems/systemId/agents/agentId/jvms/jvmId";
+        ContentResponse response = client.newRequest(url).method(HttpMethod.PUT).send();
+        assertTrue(response.getStatus() == Response.Status.SERVICE_UNAVAILABLE.getStatusCode());
+    }
+
+    @Test
+    public void testPostSystems() throws InterruptedException, ExecutionException, TimeoutException {
+        String url = baseUrl + "/namespace/systems/systemId";
+        ContentResponse response = client.newRequest(url).method(HttpMethod.POST).send();
+        assertTrue(response.getStatus() == Response.Status.SERVICE_UNAVAILABLE.getStatusCode());
+    }
+
+    @Test
+    public void testPostAgents() throws InterruptedException, ExecutionException, TimeoutException {
+        String url = baseUrl + "/namespace/systems/systemId/agents/agentId";
+        ContentResponse response = client.newRequest(url).method(HttpMethod.POST).send();
+        assertTrue(response.getStatus() == Response.Status.SERVICE_UNAVAILABLE.getStatusCode());
+    }
+
+    @Test
+    public void testPostJvms() throws InterruptedException, ExecutionException, TimeoutException {
+        String url = baseUrl + "/namespace/systems/systemId/agents/agentId/jvms/jvmId";
+        ContentResponse response = client.newRequest(url).method(HttpMethod.POST).send();
+        assertTrue(response.getStatus() == Response.Status.SERVICE_UNAVAILABLE.getStatusCode());
+    }
+
+    @Test
+    public void testDeleteSystems() throws InterruptedException, ExecutionException, TimeoutException {
+        String url = baseUrl + "/namespace/systems/systemId";
+        ContentResponse response = client.newRequest(url).method(HttpMethod.DELETE).send();
+        assertTrue(response.getStatus() == Response.Status.SERVICE_UNAVAILABLE.getStatusCode());
+    }
+
+    @Test
+    public void testDeleteAgents() throws InterruptedException, ExecutionException, TimeoutException {
+        String url = baseUrl + "/namespace/systems/systemId/agents/agentId";
+        ContentResponse response = client.newRequest(url).method(HttpMethod.DELETE).send();
+        assertTrue(response.getStatus() == Response.Status.SERVICE_UNAVAILABLE.getStatusCode());
+    }
+
+    @Test
+    public void testDeleteJvms() throws InterruptedException, ExecutionException, TimeoutException {
+        String url = baseUrl + "/namespace/systems/systemId/agents/agentId/jvms/jvmId";
+        ContentResponse response = client.newRequest(url).method(HttpMethod.DELETE).send();
+        assertTrue(response.getStatus() == Response.Status.SERVICE_UNAVAILABLE.getStatusCode());
+    }
+}
