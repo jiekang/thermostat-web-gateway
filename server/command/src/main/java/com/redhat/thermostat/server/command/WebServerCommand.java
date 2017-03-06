fa272ae9070a3007d8c9c9aa9b1d5dd707a3f7b3
Jie Kang <21.jiekang@gmail.com>
2017-03-06 14:58:09 -0500

    Add mongo username/password to WebServerCommand to match thermostat-devsetup credentials

--- server/command/src/main/java/com/redhat/thermostat/server/command/WebServerCommand.java
+++ server/command/src/main/java/com/redhat/thermostat/server/command/WebServerCommand.java
@@ -1,6 +1,8 @@
 package com.redhat.thermostat.server.command;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.redhat.thermostat.server.core.web.CoreServer;
 
@@ -8,7 +10,12 @@ import com.redhat.thermostat.server.core.web.CoreServer;
 public class WebServerCommand {
     public static void main(String[] args) {
         CoreServer server = new CoreServer();
-        server.buildServer(Collections.EMPTY_MAP, Collections.EMPTY_MAP, Collections.EMPTY_MAP);
+
+        Map<String, String> mongoConfig = new HashMap<>();
+        mongoConfig.put("MONGO_USERNAME", "mongodevuser");
+        mongoConfig.put("MONGO_PASSWORD", "mongodevpassword");
+
+        server.buildServer(Collections.EMPTY_MAP, mongoConfig, Collections.EMPTY_MAP);
 
         try {
             server.getServer().start();
