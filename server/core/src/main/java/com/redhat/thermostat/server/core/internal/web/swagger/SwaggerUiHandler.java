0cb61db1255bdb898a9ec4a029d47cbda5ede48e
Jie Kang <jkang@redhat.com>
2017-03-13 12:04:34 -0400

    Fix loading of Swagger resources when running jar. Add tests for accessing Swagger resources.
    This patch fixes the code loading the Swagger resource to also work when the resource is
    located in a jar. Tests are also added for accessing said resources.
    
    Reviewed-by: neugens
    Review-thread: http://icedtea.classpath.org/pipermail/thermostat/2017-March/022473.html

--- server/core/src/main/java/com/redhat/thermostat/server/core/internal/web/swagger/SwaggerUiHandler.java
+++ server/core/src/main/java/com/redhat/thermostat/server/core/internal/web/swagger/SwaggerUiHandler.java
@@ -37,9 +37,6 @@
 package com.redhat.thermostat.server.core.internal.web.swagger;
 
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
 
 import org.eclipse.jetty.server.handler.ResourceHandler;
@@ -51,14 +48,11 @@ public class SwaggerUiHandler {
         resourceHandler.setDirectoriesListed(false);
         resourceHandler.setWelcomeFiles(new String[]{ "index.html" });
         resourceHandler.setResourceBase("");
-        URL u = this.getClass().getResource("/swagger/index.html");
-        URI root;
-        try {
-            root = u.toURI().resolve("./").normalize();
-            resourceHandler.setBaseResource(Resource.newResource(root));
-        } catch (URISyntaxException | IOException e) {
-            return null;
-        }
+
+        URL u = this.getClass().getResource("/swagger");
+
+        resourceHandler.setBaseResource(Resource.newResource(u));
+
         return resourceHandler;
     }
 }
