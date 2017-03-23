ec18b2714949b307a86a56f8678e40f9eae51390
Severin Gehwolf <sgehwolf@redhat.com>
2017-03-23 13:54:24 +0100

    Command Channel JDK 7 compatibility. Reviewed-by: jkang Review-thread: http://icedtea.classpath.org/pipermail/thermostat/2017-March/022530.html

--- endpoints/cmd-channel/core/src/main/java/com/redhat/thermostat/server/core/internal/web/http/handlers/HtmlResourceHandler.java
+++ endpoints/cmd-channel/core/src/main/java/com/redhat/thermostat/server/core/internal/web/http/handlers/HtmlResourceHandler.java
@@ -41,7 +41,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.stream.Collectors;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -65,7 +64,13 @@ public class HtmlResourceHandler {
     private String read(InputStream stream) throws IOException {
         try (BufferedReader buffer = new BufferedReader(
                 new InputStreamReader(stream, StandardCharsets.UTF_8))) {
-            return buffer.lines().collect(Collectors.joining("\n"));
+            String line;
+            StringBuffer b = new StringBuffer();
+            while ((line = buffer.readLine()) != null) {
+                b.append(line);
+                b.append("\n");
+            }
+            return b.toString();
         }
     }
 }
