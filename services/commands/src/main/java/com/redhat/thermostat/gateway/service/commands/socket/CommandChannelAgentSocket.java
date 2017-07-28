54d4eb88fa730eed34d77bb2c3e9e00506aa8d09
Severin Gehwolf <sgehwolf@redhat.com>
2017-07-28 18:48:35 +0200

    [commands] Use logging over Debug class. Reviewed-by: jkang Review-thread: http://icedtea.classpath.org/pipermail/thermostat/2017-July/024349.html

--- services/commands/src/main/java/com/redhat/thermostat/gateway/service/commands/socket/CommandChannelAgentSocket.java
+++ services/commands/src/main/java/com/redhat/thermostat/gateway/service/commands/socket/CommandChannelAgentSocket.java
@@ -40,11 +40,14 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.websocket.PongMessage;
 import javax.websocket.Session;
 
 import com.redhat.thermostat.gateway.common.core.auth.basic.RoleAwareUser;
+import com.redhat.thermostat.gateway.common.util.LoggingUtil;
 import com.redhat.thermostat.gateway.service.commands.channel.ClientAgentCommunication;
 import com.redhat.thermostat.gateway.service.commands.channel.CommunicationsRegistry;
 import com.redhat.thermostat.gateway.service.commands.channel.model.Message;
@@ -52,6 +55,7 @@ import com.redhat.thermostat.gateway.service.commands.channel.model.WebSocketRes
 
 class CommandChannelAgentSocket extends CommandChannelSocket {
 
+    private static final Logger logger = LoggingUtil.getLogger(CommandChannelAgentSocket.class);
     private static final long SOCKET_SESSION_IDLE_TIMEOUT = TimeUnit.MINUTES.toMillis(10);
     private static final String UNKNOWN_PAYLOAD = "UNKNOWN";
     private static final String AGENT_PROVIDER_PREFIX = "thermostat-commands-provider-";
@@ -91,9 +95,9 @@ class CommandChannelAgentSocket extends CommandChannelSocket {
 
     @Override
     public void onPongMessage(PongMessage message) {
-        if (Debug.isOn()) {
+        if (logger.isLoggable(Level.FINE)) {
             String payload = extractPayload(message);
-            System.err.println("Server: Got pong message <<" + payload + ">>");
+            logger.fine("Server: Got pong message <<" + payload + ">>");
         }
     }
 
